55805d4159b235cba95b5506f00439fe591e0ce2
unknown <akbaranung06@gmail.com>
2021-12-24 17:54:17 +0700

    first commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,9 @@
 import "./Portfolio.scss";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>ini merupakan halaman portfolio</h1>
+    </div>
+  );
 }
